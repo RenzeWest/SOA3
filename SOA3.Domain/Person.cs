df35c561add84839adbacc3e405f081502fbe905
RenzeWest <rg.westerink@student.avans.nl>
2026-03-30 15:13:37 +0200

    Change to patterns

--- SOA3.Domain/Person.cs
+++ SOA3.Domain/Person.cs
@@ -1,18 +1,20 @@
-﻿namespace SOA3.Domain
+﻿using SOA3.Domain.NotificationPatterns;
+
+namespace SOA3.Domain
 {
     public class Person
     {
         private Guid _id;
         private string _name;
-        private string _email;
+        public string Email { get; private set; }
 
         public Person(string name, string email)
         {
             _name = name;
-            _email = email;
+            Email = email;
         }
 
-        //private List<NotificationChannel> _notificationPreferences = new List<NotificationChannel>();
+        public List<NotificationChannel> NotificationPreferences { get; private set; } = new List<NotificationChannel>();
 
         public string GetName() => _name;   
 
--- SOA3.Domain/PipelinePatterns/VisitorPattern/RunVisitor.cs
+++ SOA3.Domain/PipelinePatterns/VisitorPattern/RunVisitor.cs
@@ -6,18 +6,18 @@ namespace SOA3.Domain.PipelinePatterns.VisitorPattern
     {
         public void PipelineFailed(Exception exception) => Console.WriteLine($"[Run] Failed with message:{exception.Message}"); // TODO: Should fail the sprint Ig
 
-        public void VisitAnalysePipelineAction(AnalysePipelineAction action) => Console.WriteLine($"[Run] Analyse action called");
+        public void VisitAnalysePipelineAction(AnalysePipelineAction action) => Console.WriteLine($"[Run] Analyse action called: {action.Name}");
 
-        public void VisitBuildPipelineAction(BuildPipelineAction action) => Console.WriteLine($"[Run] Build action called");
+        public void VisitBuildPipelineAction(BuildPipelineAction action) => Console.WriteLine($"[Run] Build action called: {action.Name}");
 
-        public void VisitDeployPipelineAction(DeployPipelineAction action) => Console.WriteLine($"[Run] Deploy action called");
+        public void VisitDeployPipelineAction(DeployPipelineAction action) => Console.WriteLine($"[Run] Deploy action called: {action.Name}");
 
-        public void VisitPackagePipelineAction(PackagePipelineAction action) => Console.WriteLine($"[Run] Package action called");
+        public void VisitPackagePipelineAction(PackagePipelineAction action) => Console.WriteLine($"[Run] Package action called: {action.Name}");
 
-        public void VisitSourcePipelineAction(SourcePipelineAction action) => Console.WriteLine($"[Run] Source action called");
+        public void VisitSourcePipelineAction(SourcePipelineAction action) => Console.WriteLine($"[Run] Source action called: {action.Name}");
 
-        public void VisitTestPipelineAction(TestPipelineAction action) => Console.WriteLine($"[Run] Test action called");
+        public void VisitTestPipelineAction(TestPipelineAction action) => Console.WriteLine($"[Run] Test action called: {action.Name}");
 
-        public void VisitUtilityPipelineAction(UtilityPipelineAction action) => Console.WriteLine($"[Run] Utility action called");
+        public void VisitUtilityPipelineAction(UtilityPipelineAction action) => Console.WriteLine($"[Run] Utility action called: {action.Name}");
     }
 }
